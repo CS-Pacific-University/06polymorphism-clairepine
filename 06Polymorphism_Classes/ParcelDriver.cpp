8ed44ad83acc22c5a44f9a9e19b998bd4bb742bc
claire.r.pine@gmail.com <claire.r.pine@gmail.com>
2021-05-01 15:23:31 -0700

    some code in main

--- 06Polymorphism_Classes/ParcelDriver.cpp
+++ 06Polymorphism_Classes/ParcelDriver.cpp
@@ -7,11 +7,52 @@
 // Purpose:			Function calls for functions in Parcel, letter, postcard
 //              and overnight classes. Demonstrate polymorphism.
 //***************************************************************************
+#include "Parcel.h"
+#include "Letter.h"
+#include "Postcard.h"
+#include "Overnight.h"
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 
 using namespace std;
 
+// bool openFileForRead
+// print function
+void closeFileForRead(ifstream& inputFile);
+
+//***************************************************************************
+// Function:    main
+//
+// Description: Demonstrate polymorphism 
+// 
+// Parameters:  none
+//
+// Returned:    exit status
+//***************************************************************************
+
 int main() {
+
+  const string FILE_NAME = "parcels.txt";
+
+  ifstream inFile;
+
   cout << "Reached the end!\n"; 
   return EXIT_SUCCESS;
+}
+
+
+//***************************************************************************
+// Function:    closeFileForRead
+//
+// Description: closes file "parcels.txt"
+//
+// Parameters:  inputFile   -file read in from main
+//
+// Returned:    none
+//***************************************************************************
+void closeFileForRead(ifstream& inputFile) {
+
+  inputFile.close();
+
 }
